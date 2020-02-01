4d8e0c22a8a315709646fa2d88dfa600b2f535e7
alyssaschilke <arschilke@loyola.edu>
2020-02-01 03:36:39 -0500

    testing firebase connection

--- public/FireStoreConnect/index.js
+++ public/FireStoreConnect/index.js
@@ -1,4 +1,4 @@
-
+/*
 /Users/alyssaschilke/REMOVED-firebase-adminsdk-a395w-64c0c729e8.json
 
 const admin = require('firebase-admin');
@@ -10,3 +10,36 @@ admin.initializeApp({
 });
 
 let db = admin.firestore();
+*/
+
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+var firebase = require("firebase/app");
+
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require("firebase/firestore");
+
+var firebaseConfig = {
+  apiKey: "REMOVED",
+ authDomain: "REMOVED.firebaseapp.com",
+ databaseURL: "https://REMOVED.firebaseio.com",
+ storageBucket: "REMOVED.appspot.com"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+var database = firebase.database();
+
+writeUserData("userId", "fname", "lname", "email", "pwd" )
+
+function writeUserData(userId, fname, lname, email, pwd ) {
+  firebase.database().ref('users/' + userId).set({
+    username: email,
+    first_name: fname,
+    last_name: lname,
+    password: pwd
+  });
+}
