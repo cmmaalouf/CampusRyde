bd0c485d84699e1d694af95ed36e84d51860b935
alyssaschilke <arschilke@loyola.edu>
2020-02-01 18:02:12 -0500

    test

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get("/ride", function(request, response) {
 });
 
 app.get('/ride/:userId', function (req, res) {
-  response.sendFile(__dirname + "/views/ride.html?userId="+req.params.userId)
+  response.sendFile(__dirname + "/views/ride.html?userId="+req.params)
 })
 
 app.get("/ride/board", function(request, response) {
