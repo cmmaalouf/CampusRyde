cbfe01f7a36e8f6962ba22d9719658945281c86d
alyssaschilke <arschilke@loyola.edu>
2020-02-01 17:35:56 -0500

    HAHAHAHAH

--- public/FireStoreConnect/trips.js
+++ public/FireStoreConnect/trips.js
@@ -1,25 +1,3 @@
-/*
-/Users/alyssaschilke/astral-depth-266901-firebase-adminsdk-a395w-64c0c729e8.json
-
-const admin = require('firebase-admin');
-
-let serviceAccount = require('../astral-depth-266901-firebase-adminsdk-a395w-64c0c729e8.json');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-let db = admin.firestore();
-*/
-/*
-// Firebase App (the core Firebase SDK) is always required and
-// must be listed before other Firebase SDKs
-var firebase = require("firebase/app");
-
-// Add the Firebase products that you want to use
-require("firebase/auth");
-require("firebase/firestore");
-*/
 var firebaseConfig = {
   apiKey: "AIzaSyBT3OJGVbazBLVUQm_YXQWZNN5rvmWwLjU",
   authDomain: "astral-depth-266901.firebaseapp.com",
@@ -65,28 +43,4 @@ $("#submitUser").on("click", function(event) {
   $("#newFirstName").val("");
   $("#newLastName").val("");
   $("#newPassword").val("");
-});
-
-
-
-//login
-$("#loginSubmit").on("click", function(event) {
-  // Grabs user input
-  var newEmail = $("#userEmail").val().trim();
-  var newPassword = $("#userPassword").val().trim();
-
-
-  var ref = database.ref(); ref.child('users').orderByChild('email').equalTo(newEmail).once("value", function(snapshot) {
-    var user = snapshot.val();
-    var key = Object.keys(user)[0];
-    if (user[key].pwd != newPassword){
-      document.getElementById("invalid-msg").className = "text-danger";
-      event.preventDefault();
-    }
-    else{
-
-    window.location.href = "/ride/"+key;
-    }
-});
-
 });
