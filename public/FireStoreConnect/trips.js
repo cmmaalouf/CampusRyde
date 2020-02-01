ddc664ca015a79b971a5b320ca9490b213005c79
alyssaschilke <arschilke@loyola.edu>
2020-02-01 17:35:56 -0500

    HAHAHAHAH

---
+++ public/FireStoreConnect/trips.js
@@ -0,0 +1,46 @@
+var firebaseConfig = {
+  apiKey: "REMOVED",
+  authDomain: "REMOVED.firebaseapp.com",
+  databaseURL: "https://REMOVED.firebaseio.com",
+  projectId: "REMOVED",
+  storageBucket: "REMOVED.appspot.com"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+//New User
+var database = firebase.database();
+$("#submitUser").on("click", function(event) {
+  // Prevent the default form submit behavior
+  event.preventDefault();
+
+  // Grabs user input
+  var newEmail = $("#newEmail").val().trim();
+  var newFirstName = $("#newFirstName").val().trim();
+  var newLastName = $("#newLastName").val().trim();
+  var newPassword = $("#newPassword").val().trim();
+
+  var newUser = {
+    email: newEmail,
+    fname: newFirstName,
+    lname: newLastName,
+    pwd: newPassword
+  };
+
+  database.ref("users").push(newUser);
+
+  console.log(newUser.email);
+  console.log(newUser.fname);
+  console.log(newUser.lname);
+  console.log(newUser.pwd);
+
+  // Alert
+  alert("User successfully added");
+
+  // Clears all of the text-boxes
+  $("#newEmail").val("");
+  $("#newFirstName").val("");
+  $("#newLastName").val("");
+  $("#newPassword").val("");
+});
