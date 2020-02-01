8d6bba8cea9802fa4c46f98eaa264859dd39474c
alyssaschilke <arschilke@loyola.edu>
2020-02-01 09:01:04 -0500

    maybe process form

--- public/FireStoreConnect/index.js
+++ public/FireStoreConnect/index.js
@@ -22,9 +22,9 @@ require("firebase/firestore");
 
 var firebaseConfig = {
   apiKey: "REMOVED",
- authDomain: "REMOVED.firebaseapp.com",
- databaseURL: "https://REMOVED.firebaseio.com",
- storageBucket: "REMOVED.appspot.com"
+  authDomain: "REMOVED.firebaseapp.com",
+  databaseURL: "https://REMOVED.firebaseio.com",
+  storageBucket: "REMOVED.appspot.com"
 };
 
 // Initialize Firebase
@@ -32,14 +32,44 @@ firebase.initializeApp(firebaseConfig);
 
 
 var database = firebase.database();
+$("#submitUser").on("click", function(event) {
+  // Prevent the default form submit behavior
+  event.preventDefault();
 
-writeUserData("userId", "fname", "lname", "email", "pwd" );
+  // Grabs user input
+  var newEmail = $("#newEmail")
+    .val()
+    .trim();
+  var newFirstName = $("#newFirstName")
+    .val()
+    .trim();
+  var newLastName = $("#newLastName")
+    .val()
+    .trim();
+  var newPassword = $("#newPassword")
+    .val()
+    .trim();
 
-function writeUserData(userId, fname, lname, email, pwd ) {
-  firebase.database().ref('users/' + userId).set({
-    username: email,
-    first_name: fname,
-    last_name: lname,
-    password: pwd
-  });
-}
+  var newUser = {
+    email: newEmail,
+    fname: newFirstName,
+    lname: newLastName,
+    pwd: newPassword
+  };
+
+  database.ref().push(newUser);
+
+  console.log(newUser.email);
+  console.log(newUser.fname);
+  console.log(newUser.lname);
+  console.log(newUser.pwd);
+
+  // Alert
+  alert("User successfully added");
+
+  // Clears all of the text-boxes
+  $("#newEmail").val("");
+  $("#destination-input").val("");
+  $("#first-train-input").val("");
+  $("#frequency-input").val("");
+});
