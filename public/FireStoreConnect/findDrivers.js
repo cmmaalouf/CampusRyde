7dc1ed08637937c7383f5f4c08143af51f7ce0f8
Chiara <cmmaalouf@loyola.edu>
2020-02-01 17:58:06 -0500

    started find drivers function

---
+++ public/FireStoreConnect/findDrivers.js
@@ -0,0 +1,18 @@
+// Chiara Maalouf
+// February 1, 2020
+// CampusRyde 
+// Hoya Hacks 2020
+
+
+
+// connect to firebase
+
+//collect location
+//collect state
+
+//get all driver going to given state
+
+//create list of drivers:distance from my loc
+//sort list by distance
+
+
