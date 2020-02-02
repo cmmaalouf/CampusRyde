3c4462412015c1f0ef38b736ad24f475ca8284e8
Chiara <cmmaalouf@loyola.edu>
2020-02-01 21:55:47 -0500

    some updated find drivers by state w html file

--- public/FireStoreConnect/findDrivers.js
+++ public/FireStoreConnect/findDrivers.js
@@ -4,7 +4,6 @@
 // Hoya Hacks 2020
 
 
-
 // connect to firebase
 var firebaseConfig = {
 	  apiKey: "AIzaSyBT3OJGVbazBLVUQm_YXQWZNN5rvmWwLjU",
@@ -15,26 +14,44 @@ var firebaseConfig = {
 };
 
 // Initialize Firebase
- firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
 
  var database = firebase.database();
 
-$("#search").on("click",function(event){
+//$("#search").on("click",function(event){
 //collect location
 //collect state
-var city = $("#destination").val().trim()+ $("#state").val().trim();
-var state = $("#state").val().trim();
-
+//var city = $("#destination").val().trim()+ $("#state").val().trim();
+//var state = $("#state").val().trim();
+var city = 'Bayville';
+var state = 'NJ'
 //get all driver going to given state
-var ref = database.ref("trips");
-ref.orderByChild("state").equalTo("NJ").on("search", function(snapshot){
+var ref = database.ref('trips');
+ref.orderByChild('state').equalTo("NJ").on("value", function(snapshot){
   var trip = snapshot.val();
-  for(t in Object.keys(user))
+  for(t in Object.keys(trip))
   {
+	var dcity = t.city;
+	  console.log(trip);
+	var date = t.departDate;
+	var time = t.departTime;
+	var seats = t.numseats;
+	var driverId = t.driverId;
+	var fname;
+	  var driveRef = database.ref('users');
+	  var id = Object.keys(driveRef)[0];
+	  /*if id.equalTo(driverId).once("value", function(snapshot) {
+		      var user = snapshot.val();
+		      var key = Object.keys(user)[0];
+		      fname = user[key].fname;
+	  });
+		  console.log(fname); 
+		  */
+		  console.log(time);
 
 	}
 	
 });
 
-}
+//}
