7cb39986a824a810b1a818c68074d9ed94c2e94e
Chiara <cmmaalouf@loyola.edu>
2020-02-01 19:35:19 -0500

    getting drivers by same state

--- public/FireStoreConnect/findDrivers.js
+++ public/FireStoreConnect/findDrivers.js
@@ -6,13 +6,35 @@
 
 
 // connect to firebase
+var firebaseConfig = {
+	  apiKey: "AIzaSyBT3OJGVbazBLVUQm_YXQWZNN5rvmWwLjU",
+	  authDomain: "astral-depth-266901.firebaseapp.com",
+	  databaseURL: "https://astral-depth-266901.firebaseio.com",
+	  projectId: "astral-depth-266901",
+	  storageBucket: "astral-depth-266901.appspot.com"
+};
 
+// Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+
+
+ var database = firebase.database();
+
+$("#search").on("click",function(event){
 //collect location
 //collect state
+var city = $("#destination").val().trim()+ $("#state").val().trim();
+var state = $("#state").val().trim();
 
 //get all driver going to given state
+var ref = database.ref("trips");
+ref.orderByChild("state").equalTo("NJ").on("search", function(snapshot){
+  var trip = snapshot.val();
+  for(t in Object.keys(user))
+  {
 
-//create list of drivers:distance from my loc
-//sort list by distance
+	}
+	
+});
 
-
+}
