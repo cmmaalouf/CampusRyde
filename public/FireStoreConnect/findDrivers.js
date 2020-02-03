be1c57fa870163fdcac5a97c3bd95f27a258ead3
alyssaschilke <arschilke@loyola.edu>
2020-02-02 23:10:08 -0500

    hope 2

--- public/FireStoreConnect/findDrivers.js
+++ public/FireStoreConnect/findDrivers.js
@@ -6,7 +6,7 @@
 
 // connect to firebase
 var firebaseConfig = {
-  apiKey: MY_API_KEY,
+  apiKey: process.env.MY_API_KEY,
   authDomain: "REMOVED.firebaseapp.com",
   databaseURL: "https://REMOVED.firebaseio.com",
   projectId: "REMOVED",
